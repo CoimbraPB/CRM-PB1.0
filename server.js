b3652fdc2da82b40709c9bbe8167de4d42abaaf4
Coimbra10 <lucass.coimbra@hotmail.com>
2025-06-05 12:02:15 -0300

    Switch from bcrypt to bcryptjs for login validation

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Client } = require('pg');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const path = require('path');
 
 const app = express();
